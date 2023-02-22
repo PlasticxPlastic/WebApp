e9edf4fd8081e55e4f7b7c93d5e8c4503e04cc17
Merliny <pontakorn_oom@hotmail.co.th>
2023-02-23 02:28:03 +0700

    lab4_js

---
+++ lab4_js/index.js
@@ -0,0 +1,14 @@
+var prev;
+function updateDate() {
+    const color_pool = ["red","green","blue","yellow","black","white","lightgrey"];
+    var currentTime = new Date().toString();
+    var colortext = color_pool[Math.floor(Math.random() * 6)];
+    var colorbg = color_pool[Math.floor(Math.random() * 6)];
+    while(colortext == colorbg || prev == colortext){
+        colortext = color_pool[Math.floor(Math.random() * 6)];
+    }
+    postMessage([currentTime, colortext, colorbg]);
+    prev = colortext;
+    setTimeout(updateDate, 1000);
+}
+updateDate();
